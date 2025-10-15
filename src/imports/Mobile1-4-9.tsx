a8668cd7632f2eb3495b9b8954136379d82f42e2
errthdesigns <erin@erinthomson.co.uk>
2025-10-15 22:46:52 +0100

    revert: restore sticker and original coming soon position

--- src/imports/Mobile1-4-9.tsx
+++ src/imports/Mobile1-4-9.tsx
@@ -1,6 +1,15 @@
+import imgImage15 from "figma:asset/9c7741aabea6e234bce913cb017f081bbdd421e8.png";
+
 export default function Mobile1() {
   return (
     <div className="bg-[#291a01] relative size-full" data-name="mobile-1" style={{ minHeight: "100vh", width: "100%" }}>
+      <div className="absolute flex h-[calc(1px*((var(--transform-inner-width)*0.27512326836586)+(var(--transform-inner-height)*0.9614089727401733)))] items-center justify-center left-0 top-[579px] w-[calc(1px*((var(--transform-inner-height)*0.27512326836586)+(var(--transform-inner-width)*0.9614089727401733)))]" style={{ "--transform-inner-width": "334", "--transform-inner-height": "144" } as React.CSSProperties}>
+        <div className="flex-none rotate-[344.031deg]">
+          <div className="h-[144px] relative w-[334px]" data-name="image 15">
+            <img alt="" className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none size-full" src={imgImage15} />
+          </div>
+        </div>
+      </div>
       <div className="absolute flex flex-col font-sans font-bold h-[452px] justify-center leading-[normal] left-1/2 text-[#ffe7e0] text-[20px] text-center top-[calc(50%-88px)] tracking-[0.8px] translate-x-[-50%] translate-y-[-50%] w-[328px]">
         <p className="mb-0">
           <span className="font-sans font-bold text-[magenta] tracking-[0.64px] mx-[0px] my-[16px]">{`Meet Cockblock. `}</span>
@@ -25,7 +34,7 @@ export default function Mobile1() {
           {`and will always be a free tool. `}
         </p>
       </div>
-      <div className="absolute flex flex-col font-sans font-bold justify-center leading-[0] text-[20px] text-[magenta] text-center text-nowrap tracking-[2px]" style={{ left: "50%", bottom: "24px", transform: "translateX(-50%)" }}>
+      <div className="absolute flex flex-col font-sans font-bold justify-center leading-[0] left-[281px] text-[20px] text-[magenta] text-center text-nowrap top-[768.5px] tracking-[2px] translate-x-[-50%] translate-y-[-50%]">
         <p className="leading-[normal] whitespace-pre">COMING SOON...</p>
       </div>
     </div>
